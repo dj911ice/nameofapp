27c03b5ffabdd4c6c447397ab2263cf8563a1797
dj911ice <dj911ice@gmail.com>
2017-02-02 16:20:37 -0500

    5.3v8 application_mailer.rb updated and truncated

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,7 +1,5 @@
 class ApplicationMailer < ActionMailer::Base.mail
 	
-  default (from: 'from@example.com', to: => 'to@domain.com',
- 	:subject => "Welcome to My Awesome Site",
-  :body => 'I am the email body').deliever_now
+  default from: 'from@example.com'
   layout 'mailer'
 end
