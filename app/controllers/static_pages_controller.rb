ada5e1b3685853cfcbe5c652c2cc1386206b667e
dj911ice <dj911ice@gmail.com>
2017-02-03 15:18:23 -0500

    5.3v10 stylized forms and added preview mailer.

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -9,6 +9,7 @@ class StaticPagesController < ApplicationController
   	@name = params[:name]
   	@email = params[:email]
   	@message = params[:message]
+    UserMailer.contact_form(@email, @name, @message).deliver_now
   	ActionMailer::Base.mail(:from => @email,
   		:to => 'justin.p.dickerson@gmail.com',
   		:subject => "A new contact form message from #{@name}",
