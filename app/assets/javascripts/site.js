290e16fae5fc5507e4fc60d9b58d5e33e90a06f1
dj911ice <dj911ice@gmail.com>
2017-04-13 18:42:24 -0400

    6.9: Added Elevate Zoom plugin to project.

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -5,5 +5,6 @@ $(document).on('turbolinks:load', function(){
 		score: function(){
 			return $(this).attr('data-score');
 		}
-	});
+	}); //Raty Plugin
+	$('.img-zoom').elevateZoom(); //Elevate Zoom Plugin
 });
