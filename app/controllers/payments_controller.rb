b214ad14ee4ffcddc0a1742b998337f42eea51c3
dj911ice <dj911ice@gmail.com>
2017-04-12 20:56:59 -0400

    6.7 Usage of byebug in payments, comments, and products controllers.

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,6 +5,7 @@ class PaymentsController < ApplicationController
 		@user = current_user
 		token = params[:stripeToken]
 		# Create the charge on Stripe's servers - this will charge the user's card
+		byebug
 		begin
 			charge = Stripe::Charge.create(
 				:amount => (@product.price * 100).to_i, # amount in cents, again
