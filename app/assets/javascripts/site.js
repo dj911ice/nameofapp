76ae1eb9038dab32f1c326b5588577b1016774e9
dj911ice <dj911ice@gmail.com>
2017-04-19 13:59:33 -0400

    6.10: Added Ajax

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,4 +1,4 @@
-$(document).on('turbolinks:load', function(){
+var refreshRating = function(){
 	$('.rating').raty({path: '/assets', scoreName: 'comment[rating]'});
 	$('.rated').raty({path: '/assets',
 		readOnly: true,
@@ -6,5 +6,10 @@ $(document).on('turbolinks:load', function(){
 			return $(this).attr('data-score');
 		}
 	}); //Raty Plugin
+};
+
+$(document).on('turbolinks:load ajaxSuccess', function(){
+	refreshRating(); // Raty Plugin Call
+	
 	$('.img-zoom').elevateZoom(); //Elevate Zoom Plugin
 });
